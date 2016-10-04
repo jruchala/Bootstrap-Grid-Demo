ad920d7b71a3fa89e1a2f548be8e7e9683842d91
jruchala <jruchala@gmail.com>
2016-10-04 13:00:18 -0400

    initial commit of bootstrap grid system demonstration

---
+++ BootstrapGridSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BootstrapGridSystem.Startup))]
+namespace BootstrapGridSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
